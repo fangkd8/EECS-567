37ba4cfcc82e1d79f4da13dcfcae47c5e4c397a9
fangkd8 <43018640+fangkd8@users.noreply.github.com>
2018-11-03 19:03:03 -0400

    Add files via upload

--- kineval/kineval_servo_control.js
+++ kineval/kineval_servo_control.js
@@ -21,9 +21,37 @@ kineval.setpointDanceSequence = function execute_setpoints() {
     // if update not requested, exit routine
     if (!kineval.params.update_pd_dance) return; 
 
-    for (x in robot.joints){
-
+    var m=[];
+    m = kineval.params.dance_sequence_index;
+    
+    if (typeof(flag) !== 'undefined'){
+        i = t;
     }
+    else if (typeof(flag) == 'undefined'){
+        i = 0;
+    }
+    kineval.params.setpoint_target = kineval.setpoints[m[i]];
+    set = 0;
+    kkp = 0;
+    var gpa = [];
+    gpa = kineval.setpoints[m[i]];
+    for (x in robot.joints){
+        kkp = kkp+robot.joints[x].angle;
+    }
+    for(x in gpa){
+        set = set + gpa[x];
+    }
+    errorin = Math.abs(set-kkp);
+    if (errorin < 0.002){
+        t=i+1;
+        flag = true;
+    }
+    if (typeof(t) !== 'undefined'){
+        if (t == m.length){
+            t = 0;
+        }
+    }
+    
     // STENCIL: implement FSM to cycle through dance pose setpoints
 }
 
@@ -45,12 +73,12 @@ kineval.robotArmControllerSetpoint = function robot_pd_control () {
     if ((!kineval.params.update_pd)&&(!kineval.params.persist_pd)) return; 
 
     kineval.params.update_pd = false; // if update requested, clear request and process setpoint control
-    for (x in robot.joints){
-        P = robot.joints[x].servo.p_gain;
-        D = robot.joints[x].servo.d_gain;
-        P = 1;
-        robot.joints[x].error = kineval.params.setpoint_target[x]-robot.joints[x].angle;
-        robot.joints[x].control = P*robot.joints[x].error;
+    for (y in robot.joints){
+        P = robot.joints[y].servo.p_gain;
+        D = robot.joints[y].servo.d_gain;
+        P = 0.15;
+        robot.joints[y].error = kineval.params.setpoint_target[y]-robot.joints[y].angle;
+        robot.joints[y].control = P*robot.joints[y].error;
     }
     // STENCIL: implement P servo controller over joints
 }
